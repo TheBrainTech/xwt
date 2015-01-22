7e7f58a02d87553cc6fe850f48aa97cbec29eca0
Paul Schwin <pschwin@thebrain.com>
2015-01-21 18:38:01 -0800

    WIN-471: Completed custom Popover for Windows

--- Xwt.WPF/Xwt.WPFBackend/PopoverBackend.cs
+++ Xwt.WPF/Xwt.WPFBackend/PopoverBackend.cs
@@ -27,11 +27,24 @@
 using System;
 using Xwt.Backends;
 using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using System.Windows;
 
 namespace Xwt.WPFBackend
 {
 	public class PopoverBackend : Backend, IPopoverBackend
 	{
+		const int VERTICAL_MARGIN = 2;
+		const int CARET_WIDTH = 40;
+		const int CARET_HEIGHT = 10;
+		const int BORDER_RADIUS = 7;
+		const int BORDER_PADDING = 7;
+		const int BORDER_THICKNESS = 1;
+		static readonly SolidColorBrush STROKE_COLOR = Brushes.Gray;
+		static readonly SolidColorBrush FILL_COLOR = Brushes.White;
+
 		public bool IsVisible {
 			get { return this.NativeWidget.Child.IsVisible; }
 		}
@@ -40,9 +53,8 @@ namespace Xwt.WPFBackend
 			get; set;
 		}
 
-		System.Windows.Controls.Border Border {
-			get; set;
-		}
+		Grid grid;
+		Border border;
 
 		IPopoverEventSink EventSink {
 			get; set;
@@ -58,20 +70,50 @@ namespace Xwt.WPFBackend
 
 		public PopoverBackend ()
 		{
-			Border = new System.Windows.Controls.Border {
-				BorderBrush = Brushes.Black,
-				CornerRadius = new System.Windows.CornerRadius (15),
-				Padding = new System.Windows.Thickness (15),
-				BorderThickness = new System.Windows.Thickness (1)
+
+			grid = new Grid() { 
+				Margin = new Thickness(0, VERTICAL_MARGIN, 0, 0) };
+
+			string xamlCaretPath = string.Format("M 0,{0} C 0,{0} {1},{0} {2},0 C {2},0 {3},{0} {4},{0}", CARET_HEIGHT, CARET_WIDTH / 4, CARET_WIDTH / 2, CARET_WIDTH * 3 / 4, CARET_WIDTH);
+			Geometry caretGeometry = PathGeometry.Parse(xamlCaretPath);
+			Path caretPath = new Path() {
+				Stroke = STROKE_COLOR,
+				Fill = FILL_COLOR,
+				Data = caretGeometry,
+				VerticalAlignment = VerticalAlignment.Top,
+				HorizontalAlignment = HorizontalAlignment.Center
+			};
+
+			string xamlBoundaryPath = string.Format("M 0,{0} L {1},{0}", CARET_HEIGHT + 1, CARET_WIDTH);
+			Geometry boundaryGeometry = PathGeometry.Parse(xamlBoundaryPath);
+			Path boundaryPath = new Path() {
+				Stroke = FILL_COLOR,
+				Data = boundaryGeometry,
+				VerticalAlignment = VerticalAlignment.Top,
+				HorizontalAlignment = HorizontalAlignment.Center
+			};
+			
+			border = new System.Windows.Controls.Border {
+				BorderBrush = STROKE_COLOR,
+				CornerRadius = new CornerRadius(BORDER_RADIUS),
+				Padding = new Thickness(BORDER_PADDING),
+				BorderThickness = new Thickness(BORDER_THICKNESS),
+				Margin = new Thickness(0, CARET_HEIGHT - 1, 0, 0),
+				VerticalAlignment = VerticalAlignment.Top,
+				Background = FILL_COLOR
 			};
 
 			NativeWidget = new System.Windows.Controls.Primitives.Popup {
 				AllowsTransparency = true,
-				Child = Border,
+				Child = grid,
 				Placement = System.Windows.Controls.Primitives.PlacementMode.Custom,
 				StaysOpen = false,
 				Margin = new System.Windows.Thickness (10),
 			};
+
+			grid.Children.Add(border);
+			grid.Children.Add(caretPath);
+			grid.Children.Add(boundaryPath);
 
 			NativeWidget.CustomPopupPlacementCallback = (popupSize, targetSize, offset) => {
 				var location = new System.Windows.Point (targetSize.Width / 2 - popupSize.Width / 2, 0);
@@ -108,7 +150,7 @@ namespace Xwt.WPFBackend
 		public void Show (Xwt.Popover.Position orientation, Xwt.Widget reference, Xwt.Rectangle positionRect, Widget child)
 		{
 			ActualPosition = orientation;
-			Border.Child = (System.Windows.FrameworkElement)Context.Toolkit.GetNativeWidget (child);
+			border.Child = (System.Windows.FrameworkElement)Context.Toolkit.GetNativeWidget (child);
 			NativeWidget.PlacementTarget = (System.Windows.FrameworkElement)Context.Toolkit.GetNativeWidget (reference);
 			NativeWidget.IsOpen = true;
 		}
