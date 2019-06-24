e1d9994b212b138caa27a5e54c956284037f53f6
Harlan Hugh <hhugh@thebrain.com>
2019-06-24 15:13:45 -0700

    Revert unintentional changes to XamMac AssemblyInfo

--- Xwt.XamMac/Properties/AssemblyInfo.cs
+++ Xwt.XamMac/Properties/AssemblyInfo.cs
@@ -1,15 +1,15 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle("Xwt.XamMac")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Xamarin Mac Toolkit for the Xwt UI Framework")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("${AuthorCopyright}")]
+[assembly: AssemblyProduct("Xwt UI Framework")]
+[assembly: AssemblyCopyright("Xamarin, Inc (http://www.xamarin.com)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -17,10 +17,12 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyInformationalVersion("0.1.0.0-prerelease")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
 
 //[assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("")]
+
