d2cf187671b2c0e0df52c9b4bbd97d81be02836d
Harlan Hugh <hhugh@thebrain.com>
2019-06-23 20:10:05 -0700

    Convert XamMac Target Framework to Xamarin.Mac Modern
    Confirmed working with TheBrain Mac

--- Xwt.XamMac/Properties/AssemblyInfo.cs
+++ Xwt.XamMac/Properties/AssemblyInfo.cs
@@ -1,15 +1,15 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle("Xwt.XamMac")]
-[assembly: AssemblyDescription("Xamarin Mac Toolkit for the Xwt UI Framework")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Xwt UI Framework")]
-[assembly: AssemblyCopyright("Xamarin, Inc (http://www.xamarin.com)")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("${AuthorCopyright}")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -17,12 +17,10 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyInformationalVersion("0.1.0.0-prerelease")]
+[assembly: AssemblyVersion("1.0.*")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
 
 //[assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("")]
-
